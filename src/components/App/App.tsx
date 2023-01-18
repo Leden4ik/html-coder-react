ac7286ec6d2b7c6cba0901180e582fbdc7cf781f
Nikita Akimov <akimov@myprintbar.ru>
2023-01-18 10:06:04 +0300

    slider template and add button style

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import  "./_reset.scss"
 import  "./_base.scss"
 import Footer from './../Footer/Footer'
+import Slider from './../Slider/Slider'
 
 function App() {
   return (
     <main>
+      <Slider/>
       <Footer/>
     </main>
   );
---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,68 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import 'swiper/css';
+import "swiper/css/effect-fade";
+import 'swiper/css/pagination';
+import  "./_slider.scss"
+import "./../App/_button.scss";
+// import required modules
+import { Autoplay, EffectFade,  Pagination } from 'swiper';
+const Slider = () => {
+    return(
+        <Swiper
+            spaceBetween={30}
+            effect={"fade"}
+            navigation={true}
+            pagination={{
+                clickable: true,
+            }}
+            autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+            }}
+            modules={[Autoplay, EffectFade, Pagination]}
+            className="hs__slider--list"
+        >
+            <SwiperSlide className='hs__slider--item'>
+                <div className='hs__slider--content'>
+                    <div className='hs__slider--title'>
+                        Fiberglass and specialty metals are in stock across the United States
+                        <br/>
+                        Slider 1
+                    </div>
+                    <div className='hc__button__default hs__slider--button'>
+                        Shop Now 1
+                    </div>
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className='hs__slider--item'>
+                <div className='hs__slider--content'>
+                    <div className='hs__slider--title'>
+                        Fiberglass and specialty metals are in stock across the United States
+                        <br/>
+                        Slider 2
+
+                    </div>
+                    <div className='hc__button__default hs__slider--button'>
+                        Shop Now 2
+                    </div>
+                </div>
+            </SwiperSlide>
+            <SwiperSlide className='hs__slider--item'>
+                <div className='hs__slider--content'>
+                    <div className='hs__slider--title'>
+                        Fiberglass and specialty metals are in stock across the United States
+                        <br/>
+                        Slider 3
+                    </div>
+                    <div className='hc__button__default hs__slider--button'>
+                        Shop Now 3
+                    </div>
+                </div>
+            </SwiperSlide>
+        </Swiper>
+    )
+}
+export default  Slider;
