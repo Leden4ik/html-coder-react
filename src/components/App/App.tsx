16f97b5046016de0b2d5ab46769e77bce30ceb85
Nikita Akimov <akimov@myprintbar.ru>
2023-01-18 11:59:55 +0300

    add Content block

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,12 +3,15 @@ import "./_base.scss"
 import Footer from '../Footer'
 import Slider from '../Slider'
 import Categories from '../Categories'
+import Content from '../Content'
+import ContentList from '../ContentList'
 
 function App() {
     return (
         <main>
             <Slider/>
             <Categories/>
+            <Content/>
             <Footer/>
         </main>
     );
---
+++ src/components/ContentList/index.tsx
@@ -0,0 +1,22 @@
+import "./style.scss"
+
+const ContentList = () => {
+    return (
+        <section className="hc__content">
+            <div className="hc__content--box">
+                <div className="hc__box--border">
+                    <div className="hc__box--content">
+                        <div className="hc__box--title">Large Inventory Of Power Tools &amp; Accessories</div>
+                        <div className="hc__box--text">Find the Aluminum and Stainless Steel items for your project in
+                            stock and ready to ship today! Avoid project delays and long lead times in a challenging
+                            supply chain for specialty metals and non-ferrous items. We are well stocked and here to
+                            serve you with guided support.
+                        </div>
+                        <div className="hc__button__default hc__box--button">Shop Now</div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default ContentList;
